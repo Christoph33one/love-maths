003ac73402e5c5ab8278570bb3511cf7929ef56c
Christoph33one <cjbrowning23@hotmail.com>
2022-08-18 15:46:43 +0000

    add addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,23 +10,31 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked submit");
             } else {
                     let gameType = this.getAttribute("data-type");
-                    alert(`You clicked ${gameType}`);
+                    runGame(gameType);
             }
             
         })
     }
+    runGame("addition");
 })
 
 /**
  * The main game "loop" called when the script is first loaded
- * and after the users anwser ahs been processed 
+ * and after the users anwser has been processed 
  */
-function runGame () {
+function runGame (gameType) {
     // creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
+    if (gameType === "addition") {
+        displayAdditionQuestions(num1, num2);
+    } else {
+        alert(`unknown game type: ${gameType}`)
+        throw`unknow game type: ${gameType}. Aborting!`;
+    }
 }
+
 
 function checkAnswer () {
 
@@ -44,8 +52,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestions() {
-
+function displayAdditionQuestions(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+   
 }
 
 function displaySubtractQuestion() {
